8d8b8e503ac31a4f4bc12c7bfd84558e64095ca0
Shalabyyy <youshalaby@gmail.com>
2019-07-16 14:29:39 +0200

    Deployment 4

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 4000 | process.env.PORT
-const path= require('path')
-const cors = require('cors');
-const mongoose = require('mongoose')
-app.use(express.json())
+const express = require("express");
+const app = express();
+const port = 4000 | process.env.PORT;
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+app.use(express.json());
 app.use(cors());
 
-const users= require('./routes/api/users')
-const products= require('./routes/api/products')
-const orders= require('./routes/api/orders')
-const admin = require('./routes/api/admin')
-const media = require('./routes/api/uploads')
+const users = require("./routes/api/users");
+const products = require("./routes/api/products");
+const orders = require("./routes/api/orders");
+const admin = require("./routes/api/admin");
+const media = require("./routes/api/uploads");
 
-mongoose.connect('mongodb+srv://youshalaby:Youssef98%2E@arthub-xqxau.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
-mongoose.connection.once('open', function(){
-  console.log('Conection has been made!');
-}).on('error', function(error){
-    console.log('Error is: ', error);
-});
-
-app.get('/', (req, res) => res.json({msg:'Hello World'}))
-
-app.use('/api/users',users)
-app.use('/api/products',products)
-app.use('/api/orders',orders)
-app.use('/api/admin',admin)
-app.use('/api/uploads',media)
-
-app.use('/uploads',express.static('uploads'));
-
-if(process.env.NODE_ENV ==='production'){
-  app.use(express.static('client/build'))
-  app.get('*',(req,res)=>{
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+mongoose.connect(
+  "mongodb+srv://youshalaby:Youssef98%2E@arthub-xqxau.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true,
+    useCreateIndex: true
+  },
+);
+mongoose.connection
+  .once("open", function() {
+    console.log("Conection has been made!");
   })
+  .on("error", function(error) {
+    console.log("Error is: ", error);
+  });
+
+app.get("/", (req, res) => res.json({ msg: "Hello World" }));
+
+app.use("/api/users", users);
+app.use("/api/products", products);
+app.use("/api/orders", orders);
+app.use("/api/admin", admin);
+app.use("/api/uploads", media);
+
+app.use("/uploads", express.static("uploads"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
-app.listen(port, () => console.log(`Example app listening on port 4000!`))
-
+app.listen(port, () => console.log(`Example app listening on port 4000!`));
